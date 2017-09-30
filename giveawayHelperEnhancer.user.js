a8e19df768785b3f21777959f56191789c6b9b27
gekkedev <17025257+gekkedev@users.noreply.github.com>
2017-09-30 20:57:05 +0200

    made the prototype more dynamic

--- giveawayHelperEnhancer.user.js
+++ giveawayHelperEnhancer.user.js
@@ -1,17 +1,37 @@
-// ==UserScript==
-// @name         Giveaway Helper Enhancer
-// @namespace    https://github.com/gekkedev/giveawayHelperEnhancer
-// @version      0.1
-// @description  Enhances the popular Steam key giveaway site helper
-// @author       gekkedev
-// @match        *://*.marvelousga.com/index.php
-// @grant        none
-// @updateURL https://raw.githubusercontent.com/gekkedev/giveawayHelperEnhancer/master/giveawayHelperEnhancer.user.js
-// @downloadURL https://raw.githubusercontent.com/gekkedev/giveawayHelperEnhancer/master/giveawayHelperEnhancer.user.js
-// ==/UserScript==
-
 (function() {
-    if(document.location.hostname.split(".").splice(-2).join(".") == "marvelousga.com") {
-        $("ins[class*='adsbygoogle']").remove();
+    /**
+    * Determine what to do for this page based on what's defined in the "config" variable
+    *
+    * 		hostname: String
+    *			The hostname of the site we're setting the config for. Must be the same as what's defined
+    *			as @match in the metadata block above.
+    *
+    *		googleads: Boolean
+    * 			This tells us if the site we visit has ads (this refers to banners of any size). We'll then trigger the removal function for that.
+    *
+    */
+    var config = [
+        {
+            hostname: "marvelousga.com",
+            ads: true
+        },
+        {
+            hostname: "chubbykeys.com",
+            ads: false
+        }
+    ];
+    var removeGoogleAds = (function(){
+        $("ins[class*='adsbygoogle']").hide();
+        console.log("removed google ads!");
+    });
+    for(var i = 0; i < config.length; i++) {
+        var site = config[i];
+
+        if(document.location.hostname.split(".").splice(-2).join(".") == site.hostname) {
+            if (site.ads) {
+                removeGoogleAds();
+                //add other ad services here if known
+            }
+        }
     }
 })();
